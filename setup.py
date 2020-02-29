3fad8f9b7885d5364f60247eb8bba55ec65d2736
Marco-Menner <38099357+Marco-Menner@users.noreply.github.com>
2020-02-29 16:47:09 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='smurf',
+    url='https://github.com/...',
+    author='Marco Menner',
+    author_email='marco.menner88@gmail.com',
+    # Needed to actually package something
+    packages=['smurf'],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='private',
+    description='smoothing',
+)
