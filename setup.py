7050065a245021d2055bd286dbf110776808fb50
mmenner <38099357+mmenner@users.noreply.github.com>
2020-03-12 11:07:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['smurf'],
     # Needed for dependencies
-    install_requires=['numpy', 'scipy', 'gurobipy (opt)', 'cvxopt (opt)'],
+    install_requires=['numpy', 'scipy'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
