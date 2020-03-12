55c82f78e61006e8bb65106b431c323e251020a4
mmenner <38099357+mmenner@users.noreply.github.com>
2020-03-12 11:44:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='smurf',
     url='https://github.com/mmenner',
-    author='Marco Menner',
+    author='mmenner',
     author_email='marco.menner88@gmail.com',
     packages=['smurf'],
     install_requires=['numpy', 'scipy'],
