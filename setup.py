d7e973d996421a9ce68b1bc29acf4aa257517bdc
mmenner <38099357+mmenner@users.noreply.github.com>
2020-03-12 11:44:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='smurf',
     url='https://github.com/mmenner',
     author='Marco Menner',
     author_email='marco.menner88@gmail.com',
-    # Needed to actually package something
     packages=['smurf'],
-    # Needed for dependencies
     install_requires=['numpy', 'scipy'],
-    # *strongly* suggested for sharing
     version='0.1',
-    # The license can be anything you like
     license='private',
     description='smoothing',
 )
